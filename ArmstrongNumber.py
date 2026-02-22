e2f5665e3e38eab5f4a088cef6970b9cdcc37911
Gayithri <167066150+CodewithGayithri@users.noreply.github.com>
2026-02-22 19:32:20 +0530

    Create ArmstrongNumber.py

---
+++ ArmstrongNumber.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the Number: "))
+num = n
+nod = len(str(num))
+total = 0
+while num > 0:
+    last_digit = num % 10
+    total = total + (last_digit ** nod)
+    num = num // 10
+print(total == n)
