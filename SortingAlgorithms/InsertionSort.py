47ad8a0fddb81ac66e458114145df7fb8904fd27
gayithri <ngayithri22@gmail.com>
2026-03-01 20:41:53 +0530

    Insertion Sort Added

---
+++ SortingAlgorithms/InsertionSort.py
@@ -0,0 +1,14 @@
+# nums = [3,5,6,4,8,9,10,7,1]
+nums = list(map(int,input("Enter list: ").split()))
+
+def insertionSort(nums):
+    for i in range(1,len(nums)):
+        key = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > key:
+            nums[j+1] = nums[j]
+            j -= 1
+        nums[j+1] = key
+    return nums
+res = insertionSort(nums)
+print(res)
