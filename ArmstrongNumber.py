b5ed0380bc925e80582906ec91ccdab149bfc8a4
Gayithri <167066150+CodewithGayithri@users.noreply.github.com>
2026-03-04 22:04:13 +0530

    Update ArmstrongNumber.py

--- ArmstrongNumber.py
+++ ArmstrongNumber.py
@@ -1,10 +1,10 @@
 n = int(input("Enter the Number: "))
 num = n
-nod = len(str(num))
+no_of_digits = len(str(num))
 total = 0
 
 while num > 0:
     last_digit = num % 10
-    total = total + (last_digit ** nod)
+    total = total + (last_digit ** no_of_digits)
     num = num // 10
 print(total == n)
