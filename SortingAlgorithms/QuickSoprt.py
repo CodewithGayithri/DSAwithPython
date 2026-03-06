4a97b89177df3dd24a9e3fadbdb09d764db06b7f
gayithri <ngayithri22@gmail.com>
2026-03-06 20:57:19 +0530

    quicksorts s

--- SortingAlgorithms/QuickSoprt.py
+++ SortingAlgorithms/QuickSoprt.py
@@ -1,20 +1,22 @@
 def partition(nums, low, high):
     pivot = nums[low]
-    i=low, j=high
+    i=low, 
+    j=high
     while i<j:
-        while nums[i]< pivot and i<=high-1:
+        while nums[i]<pivot and i<=high-1:
             i+=1
-        while nums[j]>pivot and j <=low + 1:
+        while nums[j]>pivot and j >=low+1:
             j -= 1
         if i<j:
             nums[i], nums[j] = nums[j], nums[i]
 
     nums[low], nums[j] = nums[j], nums[low]
-    return i
-nums = list(int,input("enter The Array: "))
+    return j
+nums = list(map(int,input("enter The Array: ").split()))
 def quicksort(nums,low,high):
     if i<j:
         p_indx = partition(nums,low,high)
         quicksort(nums,low,p_indx-1)
         quicksort(nums,p_indx+1, high)
+    return nums
 
