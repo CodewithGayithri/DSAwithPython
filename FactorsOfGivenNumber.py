85b47685e29f579b4f2055a5e5cb46ad827a3f29
gayithri <ngayithri22@gmail.com>
2026-03-08 21:11:15 +0530

    optimal solution added

--- FactorsOfGivenNumber.py
+++ FactorsOfGivenNumber.py
@@ -1,10 +1,26 @@
-num = int(input("Enter Number: "))
-result = []
-def Factors(num):
-    for i in range(1,num+1):
-        if num % i == 0:
-            result.append(i)
-    return result
+# num = int(input("Enter Number: "))
+# result = []
+# def Factors(num):
+#     for i in range(1,num+1):
+#         if num % i == 0:
+#             result.append(i)
+#     return result
 
-res = Factors(num)
-print(res)
+# res = Factors(num)
+# print(res)
+
+
+#optimal Solution
+result=[]
+from math import sqrt
+def factors(num):
+    for i in range(1,int(sqrt(num))+1):
+        if num%i==0:
+            result.append(i)
+            if num//i!= i:
+                result.append(num//i)
+    result.sort()
+    return result
+num = int(input("Enter Number: "))
+res = factors(num)
+print("Factors Of Given Number: ",res)
