6f79584036343c4fe5d16b1e42d624130916474f
gayithri <ngayithri22@gmail.com>
2026-03-12 19:20:37 +0530

    used Recursion

--- StringPalindrome.py
+++ StringPalindrome.py
@@ -1,13 +1,27 @@
-user = input("Enter String: ")
-n = len(user)
+# user = input("Enter String: ")
+# n = len(user)
+# left = 0
+# right = n - 1
+# def func(user,left,right):
+#     while left < right:
+#         if user[left] != user[right]:
+#             return False
+#         left += 1
+#         right -= 1
+#     return True
+# print(func(user,left,right))
+
+#Using Recursion
+str = input("Enter String: ")
+n = len(str)
 left = 0
-right = n - 1
-def func(user,left,right):
+right = n-1
+
+def is_str_palindrome(str,left,right):
     while left < right:
-        if user[left] != user[right]:
+        if str[left] != str[right]:
             return False
-        left += 1
-        right -= 1
+        return is_str_palindrome(str,left+1,right-1)
     return True
-print(func(user,left,right))
+print(is_str_palindrome(str,left,right))
     
