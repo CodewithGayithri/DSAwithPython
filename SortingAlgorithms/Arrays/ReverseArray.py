473f87f656ccaa90924f8f1523c0598467d9da64
gayithri <ngayithri22@gmail.com>
2026-03-14 20:21:27 +0530

    first commit

--- SortingAlgorithms/Arrays/ReverseArray.py
+++ SortingAlgorithms/Arrays/ReverseArray.py
@@ -3,7 +3,7 @@
 
 arr = [int(i) for i in input("Enter The elements: ").split()]
 i = 0
-mid = len(arr)//2
+mid = len(arr)//2 
 while i<mid:
     arr[i] ,arr[len(arr)-i-1] = arr[len(arr)-i-1],arr[i]
     i +=1
