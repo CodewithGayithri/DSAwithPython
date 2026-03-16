465ee9006e4aedce2a45e975ea3e031ba45b5f31
gayithri <ngayithri22@gmail.com>
2026-03-16 19:35:51 +0530

    first commit

--- MinMaxNumber.py
+++ MinMaxNumber.py
@@ -6,7 +6,7 @@ def find(arr):
             min = n
         elif n > max:
             max = n
-    return max
+    return max,min
 arr = [2,4,10,6,7]
-res = find(arr)
-print(res)
+max,min = find(arr)
+print(f"Maximum Number is :{max}\nMinimum Number is: {min}")
