538587ab090226e30136c6cc1f51fd70736247a6
gayithri <ngayithri22@gmail.com>
2026-03-16 19:32:16 +0530

    first commit

---
+++ MinMaxNumber.py
@@ -0,0 +1,12 @@
+def find(arr):
+    min = arr[0]
+    max = arr[0]
+    for n in arr[1:]:
+        if n < min:
+            min = n
+        else:
+            max = n
+    return max
+arr = [2,4,10,6,7]
+res = find(arr)
+print(res)
